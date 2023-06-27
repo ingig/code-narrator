fc0ba6239160031fedd94b227c446187215ed069
ingig <ingigauti@gmail.com>
2023-06-27 12:19:29 +0000

    update version 1.0.17

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 
 import App from "./App";
-import ICodeNarratorConfig from "./config/ICodeNarratorConfig";
-import fs from "fs";
-import yargs from "yargs";
-import path from "path";
-import * as process from "process";
 import CliHelper from "./utils/CliHelper";
 
 (async () => {
-
     let cliHelper = new CliHelper()
     const argv = await cliHelper.getArgv();
     let userConfig = await cliHelper.getConfig(argv);
